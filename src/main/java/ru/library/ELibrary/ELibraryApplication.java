0d28f57aaad99fc8ac68ee9f02a55173abd7e7d5
Vladislav <20031911@mail.ru>
2023-03-14 10:25:13 +0300

    Initial Commit

---
+++ src/main/java/ru/library/ELibrary/ELibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.library.ELibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/library/ELibrary/ELibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.library.ELibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
