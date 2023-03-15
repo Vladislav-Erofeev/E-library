6f1e90bd4b7963faa6676b6b5affc8b3d1313c4b
Vladislav <20031911@mail.ru>
2023-03-15 10:19:41 +0300

    Добавлена страница с книгой

--- src/main/java/ru/library/ELibrary/controllers/BookController.java
+++ src/main/java/ru/library/ELibrary/controllers/BookController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.library.ELibrary.services.BooksService;
@@ -28,6 +29,11 @@ public class BookController {
         return "book/books";
     }
 
+    @GetMapping("/{id}")
+    public String book(@PathVariable("id")int id, Model model) {
+        model.addAttribute("book", booksService.getById(id));
+        return "book/showBook";
+    }
 
     //TODO сделать возможность лайкать книгу
 }
