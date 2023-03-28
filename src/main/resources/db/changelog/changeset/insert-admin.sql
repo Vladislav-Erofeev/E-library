e0768877ff0fa1a57bc6bb298c4d4e7316ef1f0f
Vladislav <20031911@mail.ru>
2023-03-28 13:31:58 +0300

    Добавлена миграция БД

---
+++ src/main/resources/db/changelog/changeset/insert-admin.sql
@@ -0,0 +1,4 @@
+--changeset vladislav:1
+
+insert into person(name, year, email, role, password, last_name, surname) values ('Admin', 2000, 'admin', 'ROLE_ADMIN',
+'$2a$10$EFgztu0NzsQ01nzXLusOl.iU766IFaxuOLi545VbJhnzj0rdU7hk6', 'ADMIN', 'ADMIN')
